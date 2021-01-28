f9fbc20fc261bf45e7c9a2c30ec9e5660aa88dab
Nate Sales <nate@natesales.net>
2021-01-28 10:01:11 -0800

    add main api

---
+++ main.py
@@ -0,0 +1,59 @@
+from typing import List
+from enum import Enum
+import logging
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rich.logging import RichHandler
+
+from pymongo import MongoClient
+
+
+# Init rich logging handler
+logging.basicConfig(level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()])
+log = logging.getLogger("rich")
+
+app = FastAPI()
+
+log.info("Connecting to MongoDB")
+db = MongoClient("localhost:27017", replicaSet="cdnv3")["cdnv3db"]
+log.info("Connected to MongoDB")
+
+# Request structures
+
+class ECAState(str, Enum):
+    """
+    ECAState defines temporary states that an ECA may be in
+    """
+    Pending = "pending"  # Not yet connected to control plane
+    Established = "established"  # Connected and expected to serve traffic
+    Faulted = "faulted"  # Something is wrong with the node
+    Draining = "draining"  # ECA has been queued for decommissioning
+
+
+class ECARole(str, Enum):
+    """
+    ECARole defines roles that an ECA might serve
+    """
+    DNS = "dns"
+    HTTPCache = "httpcache"
+
+
+class ECA(BaseModel):
+    """
+    ECA defines user-supplied parameters of an ECA
+    """
+    provider: str
+    latitude: float
+    longitude: float
+    roles: List[ECARole]
+
+
+@app.post("/ecas/new")
+async def ecas_new(eca: ECA):
+    return db["ecas"].insert_one({
+        "provider": eca.provider,
+        "latitude": eca.latitude,
+        "longitude": eca.longitude,
+        "roeles": eca.roles
+    }).inserted_id
