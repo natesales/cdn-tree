72055f0d1f3715af74e3a4590643a2ce91d101a3
Nate Sales <nate@natesales.net>
2021-02-01 10:08:29 -0800

    add cryptod service

--- crypto/cryptod.go
+++ crypto/cryptod.go
@@ -1,40 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/miekg/dns"
-	"io/ioutil"
 	"log"
+	"net/http"
 )
 
-func main() {
-	zones := []string{"example.com"}
+// Key stores all attributes for a DNSSEC signing key
+type Key struct {
+	Base           string // base key filename prefix
+	Key            string // DNSKEY
+	Private        string // private key
+	DSKeyTag       int    // DS key tag
+	DSAlgo         int    // DS algorithm
+	DSDigestType   int    // DS digest type
+	DSDigest       string // DS digest
+	DSRecordString string // full DS record in zone file format
+}
 
-	for _, zone := range zones {
-		key := &dns.DNSKEY{
-			Hdr:       dns.RR_Header{Name: dns.Fqdn(zone), Class: dns.ClassINET, Ttl: 3600, Rrtype: dns.TypeDNSKEY},
-			Algorithm: dns.ECDSAP256SHA256, Flags: 257, Protocol: 3,
-		}
-
-		priv, err := key.Generate(256)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		ds := key.ToDS(dns.SHA256)
-
-		base := fmt.Sprintf("K%s+%03d+%05d", key.Header().Name, key.Algorithm, key.KeyTag())
-		if err := ioutil.WriteFile(base+".key", []byte(key.String()+"\n"), 0644); err != nil {
-			log.Fatal(err)
-		}
-		if err := ioutil.WriteFile(base+".private", []byte(key.PrivateKeyString(priv)), 0600); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(ds.String())
-		fmt.Printf("Key tag: %d\n", ds.KeyTag)
-		fmt.Printf("Algo: %d\n", ds.Algorithm)
-		fmt.Printf("Digest Type: %d\n", ds.DigestType)
-		fmt.Printf("Digest: %s\n", ds.Digest)
+// newKey generates a new DNSSEC signing key for a zone
+func newKey(zone string) Key {
+	key := &dns.DNSKEY{
+		Hdr:       dns.RR_Header{Name: dns.Fqdn(zone), Class: dns.ClassINET, Ttl: 3600, Rrtype: dns.TypeDNSKEY},
+		Algorithm: dns.ECDSAP256SHA256, Flags: 257, Protocol: 3,
 	}
+
+	priv, err := key.Generate(256)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ds := key.ToDS(dns.SHA256)
+
+	return Key{
+		Base:           fmt.Sprintf("K%s+%03d+%05d", key.Header().Name, key.Algorithm, key.KeyTag()),
+		Key:            key.String(),
+		Private:        key.PrivateKeyString(priv),
+		DSKeyTag:       int(ds.KeyTag),
+		DSAlgo:         int(ds.Algorithm),
+		DSDigestType:   int(ds.DigestType),
+		DSDigest:       ds.Digest,
+		DSRecordString: ds.String(),
+	}
+}
+
+func handleNewKey(w http.ResponseWriter, r *http.Request) {
+	key := newKey("example.com")
+	jsonResponse, err := json.Marshal(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func main() {
+	http.HandleFunc("/newkey", handleNewKey)
+	fmt.Println("Starting cryptod HTTP listener")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
