801365f80f2d9569bd4f764f81db70dd1228e807
Nate Sales <nate@natesales.net>
2021-02-02 22:28:59 -0800

    gateway: add client node logic

--- client/connection.py
+++ client/connection.py
@@ -1,20 +1,53 @@
 import socketio
 
 sio = socketio.Client()
-sio.connect("http://localhost:8000", headers={"X-Packetframe-ECA-Auth": "test123"})
+
+eca_id = "601496f63d405ca2e88d8c6e"
+
+print("Connecting to control plane")
+try:
+    sio.connect("http://localhost:8000", headers={"X-Packetframe-ECA-Auth": eca_id})
+except socketio.exceptions.ConnectionError as e:
+    print(e)
+    exit(1)
+else:
+    print("Connected to control plane")
 
 
 @sio.event
 def connect():
     print("connected")
-    print('my sid is', sio.sid)
 
 
 @sio.event
+def disconnect():
+    print("disconnected")
+    sio.disconnect()
+
+
+@sio.on
 def message(data):
-    print('I received a message!')
+    print(data)
 
 
-@sio.on('my message')
-def on_message(data):
-    print('I received a message!')
+@sio.on("metadata")
+def on_metadata(data):
+    print("metadata", data)
+
+
+@sio.on("global_ping")
+def on_global_ping():
+    sio.emit("global_pong")
+    print("ping")
+
+
+@sio.on("terminate")
+def on_reject(data):
+    """
+    Forcefully terminate the connection
+    :param data:
+    :return:
+    """
+    print("terminating connection", data)
+    sio.disconnect()
+    exit(2)
