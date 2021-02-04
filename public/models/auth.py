72c965902aa71d32ab1b35c15ef6c476dc1fef8d
Nate Sales <nate@natesales.net>
2021-02-04 10:20:13 -0800

    remove legacy python api

--- public/models/auth.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-
-class User(BaseModel):
-    """
-    User stores information about a user
-    """
-    email: EmailStr  # Email address
-    password: str  # Hashed password
