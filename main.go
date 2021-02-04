6ced1e6240e1a1b80069edf302bc6339db268402
Nate Sales <nate@natesales.net>
2021-02-03 20:01:59 -0800

    api: remove old socket transport code

--- main.go
+++ main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
-	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
-	socketio "github.com/googollee/go-socket.io"
-	"github.com/natesales/cdnv3/internal/database"
-	"github.com/natesales/cdnv3/internal/transport"
-	"github.com/natesales/cdnv3/internal/types"
 	log "github.com/sirupsen/logrus"
 	"time"
+
+	"github.com/natesales/cdnv3/internal/database"
+	"github.com/natesales/cdnv3/internal/types"
 )
 
 var (
-	sio *socketio.Server // Socket.IO server
-	db  *database.Database
+	db *database.Database
 )
 
 // HTTP endpoint handlers
-
-// handlePing emits a ping message to all ECAs
-func handlePing(ctx *fiber.Ctx) error {
-	log.Println("Sending global ping")
-	sio.BroadcastToRoom("/", "global", "global_ping")
-	return ctx.SendString("Sent global ping")
-}
-
-// handleConnections shows a list of nodes and their last message timestamp
-func handleConnections(ctx *fiber.Ctx) error {
-	log.Println("Getting connections")
-	sio.ForEach("/", "global", func(s socketio.Conn) {
-		lastMessage := time.Since(time.Unix(s.Context().(int64), 0)).Truncate(time.Millisecond) // assert context type to int64, parse as UNIX timestamp, compute time since then, and truncate to milliseconds
-		log.Printf("%s last message %s\n", transport.GetAuthKey(s), lastMessage)
-	})
-	return ctx.SendString("Displayed connections")
-}
 
 // handleAddNode handles a HTTP POST request to add a new node
 func handleAddNode(ctx *fiber.Ctx) error {
@@ -86,27 +66,7 @@ func main() {
 
 	db = database.New("mongodb://localhost:27017")
 
-	// Create a new socket.io server
-	sio, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Setup Socket.IO handlers
-	transport.SetupHandlers(sio, db)
-
-	// start socket.io handler
-	log.Println("Starting socket server goroutine")
-	//goland:noinspection GoUnhandledErrorResult
-	go sio.Serve()
-	//goland:noinspection GoUnhandledErrorResult
-	defer sio.Close()
-
 	app := fiber.New()
-	app.Get("/ping", handlePing)
-	app.Get("/connections", handleConnections)
-	app.All("/socket.io/", adaptor.HTTPHandler(sio))
-
 	app.Post("/nodes/add", handleAddNode)
 	app.Post("/zones/add", handleAddZone)
 
