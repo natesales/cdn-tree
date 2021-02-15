377597d7aa85753504763115fa3c8b926e9d2e4f
Nate Sales <nate@natesales.net>
2021-02-14 21:01:34 -0800

    add internal/bgp package

--- go.mod
+++ go.mod
@@ -3,10 +3,24 @@ module github.com/natesales/cdn-tree
 go 1.15
 
 require (
+	github.com/armon/go-radix v1.0.0 // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
+	github.com/eapache/channels v1.1.0 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gofiber/fiber/v2 v2.4.1
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.4.2
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/miekg/dns v1.1.38
+	github.com/osrg/gobgp v2.0.0+incompatible
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b // indirect
 	github.com/sirupsen/logrus v1.7.0
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/vishvananda/netlink v1.1.0 // indirect
+	go.etcd.io/etcd v0.5.0-alpha.5.0.20210209230119-3be9460ddc8e // indirect
 	go.mongodb.org/mongo-driver v1.4.6
+	go.uber.org/zap v1.16.0 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	google.golang.org/grpc v1.35.0 // indirect
 )
---
+++ internal/bgp/bgp.go
@@ -0,0 +1,106 @@
+package bgp
+
+import (
+	"context"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+	api "github.com/osrg/gobgp/api"
+	gobgp "github.com/osrg/gobgp/pkg/server"
+	log "github.com/sirupsen/logrus"
+	"net"
+)
+
+type Daemon struct {
+	BgpServer *gobgp.BgpServer
+}
+
+type Config struct {
+	Asn      uint32
+	RouterId string
+}
+
+func (d *Daemon) NewPeer(address string, asn uint32) error {
+	return d.BgpServer.AddPeer(context.Background(), &api.AddPeerRequest{
+		Peer: &api.Peer{
+			Conf: &api.PeerConf{
+				NeighborAddress: address,
+				PeerAs:          asn,
+			},
+		},
+	})
+}
+
+func (d Daemon) NewRoute(prefix net.IPNet, nexthop net.IPAddr) error {
+	// Determine prefix address family and set AFI (SAFI is always UNICAST)
+	var afi api.Family_Afi
+	if prefix.IP.To4() == nil { // If IPv6
+		afi = api.Family_AFI_IP6
+	} else {
+		afi = api.Family_AFI_IP
+	}
+
+	family := &api.Family{
+		Afi:  afi,
+		Safi: api.Family_SAFI_UNICAST,
+	}
+
+	// Get prefix netmask
+	_, pfxLen := prefix.Mask.Size()
+
+	// BGP route attributes
+	// TODO: error handling instead of ignoring
+	nlri, _ := ptypes.MarshalAny(&api.IPAddressPrefix{
+		Prefix:    prefix.IP.String(),
+		PrefixLen: uint32(pfxLen),
+	})
+
+	originAttr, _ := ptypes.MarshalAny(&api.OriginAttribute{
+		Origin: 0,
+	})
+
+	reachabilityAttrs, _ := ptypes.MarshalAny(&api.MpReachNLRIAttribute{
+		Family:   family,
+		NextHops: []string{nexthop.String()},
+		Nlris:    []*any.Any{nlri},
+	})
+
+	largeCommunities, _ := ptypes.MarshalAny(&api.LargeCommunitiesAttribute{
+		Communities: []*api.LargeCommunity{},
+	})
+
+	pathAttrs := []*any.Any{originAttr, reachabilityAttrs, largeCommunities}
+
+	_, err := d.BgpServer.AddPath(context.Background(), &api.AddPathRequest{
+		Path: &api.Path{
+			Family: family,
+			Nlri:   nlri,
+			Pattrs: pathAttrs,
+		},
+	})
+
+	return err
+}
+
+func Setup(asn uint32, routerId string) *Daemon {
+	s := gobgp.NewBgpServer()
+	//go s.Serve()
+
+	// global configuration
+	if err := s.StartBgp(context.Background(), &api.StartBgpRequest{
+		Global: &api.Global{
+			As:         asn,
+			RouterId:   routerId,
+			ListenPort: 179,
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	// monitor the change of the peer state
+	if err := s.MonitorPeer(context.Background(), &api.MonitorPeerRequest{}, func(p *api.Peer) { log.Info(p) }); err != nil {
+		// TODO: Send to metrics system
+		log.Fatal(err)
+	}
+
+	return &Daemon{BgpServer: s}
+}
