0d78b092dbb5674623594ad70ec408289f98d465
Nate Sales <nate@natesales.net>
2021-02-16 01:53:18 -0800

    validation: add multi-validator Register function

--- internal/validation/validation.go
+++ internal/validation/validation.go
@@ -3,16 +3,32 @@ package validation
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/miekg/dns"
+
+	"github.com/natesales/cdn-tree/internal/util"
 )
 
 // Register registers custom DNS validation handlers with the validator
 func Register(validate *validator.Validate) error {
-	return validate.RegisterValidation("dns-rr", validateDnsLabel)
+	// Map of validation functions
+	validators := map[string]func(fl validator.FieldLevel) bool{
+		"dns-rr": validateDnsLabel,
+		"region": validateRegion,
+	}
+
+	// Register all validators
+	for name, function := range validators {
+		err := validate.RegisterValidation(name, function)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil // nil error
 }
 
 // Validation functions
 
-// validateDnsLabel
+// validateDnsLabel attempts to create a DNS RR and returns if it succeeded or not
 func validateDnsLabel(fl validator.FieldLevel) bool {
 	_, err := dns.NewRR(fl.Field().String()) // ignore resulting RR
 	return err != nil
